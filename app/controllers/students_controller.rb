7e9f245945a0c5e5742dd08d7848da7d9e028315
kudlamanju <manjunathksbhat@gmail.com>
2020-08-23 10:38:19 +0530

    added score for students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -69,6 +69,6 @@ class StudentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def student_params
-      params.require(:student).permit(:full_name, :address, :phone, :institution_id)
+      params.require(:student).permit(:score, :full_name, :address, :phone, :institution_id)
     end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,4 @@
 class Student < ApplicationRecord
   belongs_to :institution
+  validates :score, numericality: { only_integer: true, message: ": Only Numerical no's are valied"}
 end
